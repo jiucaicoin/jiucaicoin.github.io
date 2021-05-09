352e1f2453a7e5d944278f68174a71b194ccefc4
shibadev <shibadev@gmail.com>
2021-05-09 05:46:08 -0700

    init

---
+++ precache-manifest.9733b1a6158c10b34adc120a8a7260e1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "11ddb396bce601344c55",
+    "url": "/static/css/main.a53eeccd.chunk.css"
+  },
+  {
+    "revision": "11ddb396bce601344c55",
+    "url": "/static/js/main.11ddb396.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "8317f9c8fa77393deb8d",
+    "url": "/static/css/2.e8cbe989.chunk.css"
+  },
+  {
+    "revision": "8317f9c8fa77393deb8d",
+    "url": "/static/js/2.8317f9c8.chunk.js"
+  },
+  {
+    "revision": "b6ad90a20b11cdfd6899f85176ddd04a",
+    "url": "/static/media/shibaIcon.b6ad90a2.png"
+  },
+  {
+    "revision": "c2dc09773dbcfc18fd7bb78fda79a19c",
+    "url": "/static/media/logo.c2dc0977.svg"
+  },
+  {
+    "revision": "960fa7fd0a36f939c73d1cad973ad40a",
+    "url": "/static/media/bg.960fa7fd.png"
+  },
+  {
+    "revision": "78baf9b6cff11c236e5998edaef38b8d",
+    "url": "/static/media/shibaOnEarth.78baf9b6.png"
+  },
+  {
+    "revision": "23c86de434c3f2204dda83f91de26eff",
+    "url": "/static/media/bowknot.23c86de4.svg"
+  },
+  {
+    "revision": "af5b291c71407d0f8154fe2d8a0bae0d",
+    "url": "/static/media/sun.af5b291c.png"
+  },
+  {
+    "revision": "d04de598265816355a95d06476c6464e",
+    "url": "/static/media/pic2.d04de598.png"
+  },
+  {
+    "revision": "a6899fb5adb978c189d225ffc4bea63f",
+    "url": "/static/media/pic1.a6899fb5.svg"
+  },
+  {
+    "revision": "cc664e00584591261681d56789f736e7",
+    "url": "/static/media/facebook.cc664e00.svg"
+  },
+  {
+    "revision": "3683b7b541f3468a4ca6b316a1621174",
+    "url": "/static/media/telegram.3683b7b5.svg"
+  },
+  {
+    "revision": "e26996ca0db95c70e9904116c71fe177",
+    "url": "/static/media/twitter.e26996ca.svg"
+  },
+  {
+    "revision": "baba94a5513fbf7c291a2c833e189082",
+    "url": "/static/media/medium.baba94a5.svg"
+  },
+  {
+    "revision": "4111109d345450d859e226d6741f741a",
+    "url": "/static/media/github.4111109d.svg"
+  },
+  {
+    "revision": "05a38a65acf9786bb8df3c96f2f8767d",
+    "url": "/static/media/reddit.05a38a65.svg"
+  },
+  {
+    "revision": "307e0e72f189c9eeb2fbdfe4f22a4ec4",
+    "url": "/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.9733b1a6158c10b34adc120a8a7260e1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
